837470166d99529c18262a351c25dcca87962de0
gitSnake <aaron@defaults-MacBook-Pro.local>
2018-05-24 18:37:08 -0400

    Added Too Big and Too Small if statements

--- coding-basics/guessing.py
+++ coding-basics/guessing.py
@@ -1,7 +1,13 @@
 import random
+
 n=random.randint(1,20)
 g=0
+
 while(g!=n):
     g=input('Guess my number ')
     g=int(g)
+    if(g>n):
+        print('Too big')
+    if(g<n):
+        print('Too small')
 print('Correct!')
