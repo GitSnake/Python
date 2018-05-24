c614c4a11e32fad24aa6f83c57dff0f9d7d933e2
gitSnake <aaron@defaults-MacBook-Pro.local>
2018-05-24 18:57:40 -0400

    Add turtle speed to 0

--- coding-basics/lines.py
+++ coding-basics/lines.py
@@ -3,27 +3,4 @@ import random
 
 cols=['blue','green','red','yellow']
 
-print(random.choice(cols))
-
-
-
-
-
-- start a loop
-
-
-
-
-
-- pick random x and y values for the end point of the line you have drawn
-
-
-
-
-- pick a random color from the list
-
-
-
-
-
-- draw a line in the chosen color to the random end point
+turtle.speed(0)
