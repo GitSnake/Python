9f5632eac9c823618b3eade43294183af16f9e75
gitSnake <aaron@defaults-MacBook-Pro.local>
2018-05-24 18:43:28 -0400

    Sudo code planning the program

---
+++ coding-basics/lines.py
@@ -0,0 +1,6 @@
+- import the libraries needed
+- make a list of colors
+- start a loop
+- pick random x and y values for the end point of the line you have drawn
+- pick a random color from the list
+- draw a line in the chosen color to the random end point
