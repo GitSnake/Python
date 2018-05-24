4c51dd950c07c99f94662f338e3fa22137b6d9ad
gitSnake <aaron@defaults-MacBook-Pro.local>
2018-05-24 18:58:38 -0400

    Add for loop to 200 with random position and random color 100 times

--- coding-basics/lines.py
+++ coding-basics/lines.py
@@ -4,3 +4,9 @@ import random
 cols=['blue','green','red','yellow']
 
 turtle.speed(0)
+
+for c in range(100):
+    x=random.randint(-200,200)
+    y=random.randint(-200,200)
+    turtle.color(random.choice(cols))
+    turtle.goto(x,y)
