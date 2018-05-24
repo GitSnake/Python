94fe31d95ac6615dce300ec26a13f8f35214d4de
gitSnake <aaron@defaults-MacBook-Pro.local>
2018-05-24 18:52:16 -0400

    imported libraries and created an array of colors

--- coding-basics/lines.py
+++ coding-basics/lines.py
@@ -1,6 +1,29 @@
-- import the libraries needed
-- make a list of colors
+import turtle
+import random
+
+cols=['blue','green','red','yellow']
+
+print(random.choice(cols))
+
+
+
+
+
 - start a loop
+
+
+
+
+
 - pick random x and y values for the end point of the line you have drawn
+
+
+
+
 - pick a random color from the list
+
+
+
+
+
 - draw a line in the chosen color to the random end point
